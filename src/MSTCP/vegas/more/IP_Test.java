5f7bc5bd3d613feb5ad790be3a66040c1928897d
ndavison21 <natemail@mail.com>
2017-03-29 18:19:13 +0100

    testing getting IP address

--- src/MSTCP/vegas/more/IP_Test.java
+++ src/MSTCP/vegas/more/IP_Test.java
@@ -4,15 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class IP_Test {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws UnknownHostException, SocketException {
         System.out.println("InetAddress.getLoopbackAddress() " + InetAddress.getLoopbackAddress().getHostAddress());
         System.out.println("InetAddress.getLocalHost() " + InetAddress.getLocalHost().getHostAddress());
         
+        Enumeration<NetworkInterface> ee = NetworkInterface.getNetworkInterfaces();
+        while (ee.hasMoreElements()) {
+            Enumeration<InetAddress> eee = ee.nextElement().getInetAddresses();
+            while (eee.hasMoreElements())
+                System.out.println("Network Interface " + eee.nextElement().getHostAddress());
+        }
         try {
             URL req = new URL("http://checkip.amazonaws.com");
             BufferedReader in = new BufferedReader(new InputStreamReader(req.openStream()));
