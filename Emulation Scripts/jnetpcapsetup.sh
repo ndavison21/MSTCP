0b8a750366d62ac03164d37c653735c5ddc4a6e0
ndavison21 <natemail@mail.com>
2017-04-18 11:39:20 +0100

    correction

--- Emulation Scripts/jnetpcapsetup.sh
+++ Emulation Scripts/jnetpcapsetup.sh
@@ -3,7 +3,7 @@ mv jnetpcap-1.3.0-1.ubuntu.x86_64.tgz ../..
 cd ../..
 tar xzvf jnetpcap-1.3.0-1.ubuntu.x86_64.tgz
 cd jnetpcap-1.3.0
-cp lbjnetpcap.so /usr/lib
+cp libjnetpcap.so /usr/lib
 cp jnetpcap.jar /usr/share/java
 echo "CLASSPATH=\".:/usr/share/java/jnetpcap.jar\"" >> /etc/environment
 echo "LD_LIBRARY_PATH=\"/usr/lib\"" >> /etc/environment
